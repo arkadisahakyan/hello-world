6d049b0d250a728eb68f6821d8ddce9cb47d32bf
Arkady Sahakyan <arkadijsaakyan3@gmail.com>
2021-09-04 10:10:29 +0400

    Added Greeting class

---
+++ src/Greeting.java
@@ -0,0 +1,16 @@
+public class Greeting {
+
+    private final String name;
+
+    public Greeting() {
+        name = "World";
+    }
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.print("Hello, " + name + "\n");
+    }
+}
